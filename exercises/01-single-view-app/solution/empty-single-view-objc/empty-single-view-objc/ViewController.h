388eb82e5dffd98ea8ddda85856c1f187000b70c
Hy-tsoung Chang <hytsoung.chang@gmail.com>
2016-01-15 22:02:36 +0100

    replaced exo 01 swift solution by correct objective-c solution

---
+++ exercises/01-single-view-app/solution/empty-single-view-objc/empty-single-view-objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  empty-single-view-objc
+//
+//  Created by Hy-tsoung Chang on 15/01/2016.
+//  Copyright © 2016 Hy-tsoung Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ exercises/01-single-view-app/solution/empty-single-view-objc/empty-single-view-objc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  empty-single-view-objc
+//
+//  Created by Hy-tsoung Chang on 15/01/2016.
+//  Copyright © 2016 Hy-tsoung Chang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
